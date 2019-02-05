a5c3e5785a2d3ad76e14eebded6fa46fd0131d07
Saptaswa Sarkar <saptaswa16@gmail.com>
2019-02-05 17:53:52 +0530

    leave_mailer
    mail facility for leaves

--- app/controllers/time_offs_controller.rb
+++ app/controllers/time_offs_controller.rb
@@ -34,6 +34,7 @@ class TimeOffsController < ApplicationController
   # POST /time_offs.json
   def create
     @time_off = TimeOff.new(time_off_params)
+    LeaveMailer.with(user: @user).leave_email.deliver_later
 
     respond_to do |format|
       if @time_off.save
---
+++ app/mailers/leave_mailer.rb
@@ -0,0 +1,9 @@
+class LeaveMailer < ApplicationMailer
+	default from: 'http://localhost:3000/users/sign_in'
+ 
+  def leave_email
+    @user = params[:user]
+    @url  = 'http://localhost:3000/users/sign_in'
+    mail(to: @user.leave_email, subject: 'Application for leave')
+  end
+end
